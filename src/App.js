9c6e7c6774948a9a0b4c35ab2f9ecac75a217349
cinnamonjs <[p.thunwattanakul@gmail.com]>
2021-11-18 21:22:07 +0700

    add navbar and content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-/******
-sad :< makmak
-******/
+import React from 'react';
+import Navbar from './navbar/navbar';
+import Content from './content/content';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Content />
       <header className="App-header">Hello world</header>
     </div>
   );
 }
-
 export default App;
-test
